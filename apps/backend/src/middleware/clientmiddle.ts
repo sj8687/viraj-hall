791044f6104625022e20fe3a921057d89bc196db
sj8687 <shreyashjadhav59807@gmail.com>
2025-10-18 00:18:22 +0530

    fix cookie

--- apps/backend/src/middleware/clientmiddle.ts
+++ apps/backend/src/middleware/clientmiddle.ts
@@ -14,53 +14,49 @@ export const userMiddleware = async (
   res: Response,
   next: NextFunction
 ): Promise<void> => {
+  try {
+const token = req.headers.authorization?.split(" ")[1];    
 
 
-  console.log(req);
-  next()
-//   try {
-// const token = req.headers.authorization?.split(" ")[1];    
-
-
-//     console.log("Checking cookies:", token);
+    console.log("Checking cookies:", token);
     
-//     if (!token) {
-//        res.status(401).json({ message: "No cookies found" });
-//        return
-//     }
+    if (!token) {
+       res.status(401).json({ message: "No cookies found" });
+       return
+    }
 
    
 
-//     const decoded = await decode({
-//       token,
-//       secret: process.env.AUTH_SECRET!,
-//       salt: process.env.AUTH_SECRETT!, 
-//     });
+    const decoded = await decode({
+      token,
+      secret: process.env.AUTH_SECRET!,
+      salt: process.env.AUTH_SECRETT!, 
+    });
 
-//     if (!decoded || !decoded.email) {
-//        res.status(403).json({ message: "Unauthorized: Invalid session" });
-//        return
-//     }
+    if (!decoded || !decoded.email) {
+       res.status(403).json({ message: "Unauthorized: Invalid session" });
+       return
+    }
 
-//     const users = await prisma.user.findUnique({
-//       where: { email: decoded.email },
-//     });
+    const users = await prisma.user.findUnique({
+      where: { email: decoded.email },
+    });
 
-//     if (!users) {
-//        res.status(403).json({ message: "User not found in database" });
-//        return
-//     }
-//     console.log(decoded.email);
-//     console.log(users.id);
+    if (!users) {
+       res.status(403).json({ message: "User not found in database" });
+       return
+    }
+    console.log(decoded.email);
+    console.log(users.id);
     
     
-//     req.email = decoded.email;
-//     req.userid = users.id;
+    req.email = decoded.email;
+    req.userid = users.id;
 
     
-//     next();
-//   } catch (err) {
-//     console.error("Session decode error:", err);
-//     res.status(401).json({ message: "Invalid session token" });
-//   }
+    next();
+  } catch (err) {
+    console.error("Session decode error:", err);
+    res.status(401).json({ message: "Invalid session token" });
+  }
 };
--- apps/backend/src/middleware/token.ts
+++ apps/backend/src/middleware/token.ts
@@ -6,45 +6,42 @@ export const middleware = async (
   res: Response,
   next: NextFunction
 ): Promise<void> => {
-
-  console.log(req);
-  next()
-//   try {
-// const token = req.headers.authorization?.split(" ")[1];
+  try {
+const token = req.headers.authorization?.split(" ")[1];
     
-//     if (!token) {
-//       res.status(401).json({ message: "No cookies found" });
-//       return;
-//     }
+    if (!token) {
+      res.status(401).json({ message: "No cookies found" });
+      return;
+    }
 
     
-//     console.log(token);
+    console.log(token);
     
 
-//     if (!token) {
-//       res.status(401).json({ message: "No auth token found" });
-//       return;
-//     }
+    if (!token) {
+      res.status(401).json({ message: "No auth token found" });
+      return;
+    }
 
-//     const decoded = await decode({
-//       token:token,
-//       salt: process.env.AUTH_SECRETT!,
-//       secret: process.env.AUTH_SECRET!
-//     });
+    const decoded = await decode({
+      token:token,
+      salt: process.env.AUTH_SECRETT!,
+      secret: process.env.AUTH_SECRET!
+    });
 
-//     console.log(decoded);
+    console.log(decoded);
     
 
-//     if (!decoded || decoded.email !== "shreyashjadhav59807@gmail.com") {
-//       res.status(403).json({ message: "Unauthorized" });
-//       return;
-//     }
+    if (!decoded || decoded.email !== "shreyashjadhav59807@gmail.com") {
+      res.status(403).json({ message: "Unauthorized" });
+      return;
+    }
 
-//     (req as any).session = decoded;
+    (req as any).session = decoded;
 
-//     next(); 
-//   } catch (err) {
-//     console.error("Session decode error:", err);
-//     res.status(401).json({ message: "Invalid session token" });
-//   }
+    next(); 
+  } catch (err) {
+    console.error("Session decode error:", err);
+    res.status(401).json({ message: "Invalid session token" });
+  }
 };
